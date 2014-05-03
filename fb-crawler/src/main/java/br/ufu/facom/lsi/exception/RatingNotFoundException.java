4a039600dc1ca0ccdbd2eba3085042a6d10ce8eb
klerisson <klerissonpaixao@gmail.com>
2014-05-03 02:53:10 -0300

    still missing duplicated bean loading fixes

---
+++ fb-crawler/src/main/java/br/ufu/facom/lsi/exception/RatingNotFoundException.java
@@ -0,0 +1,10 @@
+package br.ufu.facom.lsi.exception;
+
+public class RatingNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2024208919891924459L;
+
+}
